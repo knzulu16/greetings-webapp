c603d20bba18dbebdd0153439fcc76315f21afda
Nonzulu <knzulu16@gmail.com>
2017-05-23 09:44:56 +0200

    save what i have done

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 var express = require('express');
+var greetingsCount = 0;
+
 var app = express();
+var namesGreeted='';
+var greeted=[];
+
 
 app.get('/', function(req, res){
     res.send("Hello, Kala!")
 });
-//start the server
-const port=8000;
-app.listen(port, function(){
-  console.log('web app started on port:'+port);
 
+app.get('/Greetings/:names', function(req, res){
+  greeted.push({'name':req.params.names});
+  res.send("Greetings: " + req.params.names);
+});
+
+app.get('/Greeted', function(req, res){
+  var name = req.params.name;
+  var Greetings={};
+  for(var i=0;i<greeted.length;i++){
+    var greetedNames=greeted[i];
+    // if(greetedNames[name]===undefined){
+
+  }
+
+  res.send(greeted);
 });
 
 
+app.get('/Counter/:name', function(req, res){
+var name = req.params.name;
+var greetingsCount = 0;
+for(var i=0;i<greeted.length;i++){
+  if(greeted[i].name === name){
+    greetingsCount++;
+}
 
+  }
 
+    res.send("Has been greeted" +' '+ greetingsCount +' ' +"times");
 
+});
 
+const port=8000;
+app.listen(port, function(){
+console.log('web app started on port:'+port);
 
-
-
-
-
+});
 
 
 
